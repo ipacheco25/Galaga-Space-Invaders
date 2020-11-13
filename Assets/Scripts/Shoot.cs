d88ac265cd321f5824fa67d1fa0107c0aae5661c
Isaac Pacheco <ipacheco3354@sdsu.edu>
2020-11-12 19:36:01 -0800

    Initial commit

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public Transform laserSpawn;
+
+    public GameObject projectile;
+
+    public float nextFire = 1.0f;
+    public float currentTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserSpawn = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shoot();
+        
+    }
+
+    void shoot()
+    {
+        currentTime += Time.deltaTime;
+
+        if(Input.GetButton("Fire1") && currentTime > nextFire)
+        {
+            nextFire += currentTime;
+
+            Instantiate(projectile, laserSpawn.position, Quaternion.identity);
+
+            nextFire -= currentTime;
+            currentTime = 0.0f;
+        }
+    }
+}
