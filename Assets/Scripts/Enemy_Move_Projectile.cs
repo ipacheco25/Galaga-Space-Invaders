8145a7e28a28597f397dfea32f5bfdf83c2a4a5f
Isaac Pacheco <ipacheco3354@sdsu.edu>
2020-11-12 20:56:25 -0800

    added enemy shooting

--- Assets/Scripts/Enemy_Move_Projectile.cs
+++ Assets/Scripts/Enemy_Move_Projectile.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveProjectile : MonoBehaviour
+public class Enemy_Move_Projectile : MonoBehaviour
 {
     public Rigidbody2D projectile;
 
-    public float moveSpeed = 20.0f;
+    public float moveSpeed = 10.0f;
 
 
 
@@ -19,6 +19,6 @@ public class MoveProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        projectile.velocity = new Vector2(0,1) * moveSpeed;
+        projectile.velocity = new Vector2(0,-1) * moveSpeed;
     }
 }
---
+++ Assets/Scripts/Enemy_Shoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Shoot : MonoBehaviour
+{
+    public Transform laserSpawn;
+
+    public GameObject projectile;
+
+    public float nextFire = 1.0f;
+    public float currentTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserSpawn = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy_shoot();
+        
+    }
+
+    void enemy_shoot()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime > nextFire)
+        {
+            nextFire += currentTime;
+
+            Instantiate(projectile, laserSpawn.position, Quaternion.identity);
+
+            nextFire -= currentTime;
+            currentTime = 0.0f;
+        }
+    }
+}
